03381bb4180f930abad77e77438876f3e784b1aa
Nicolas GRAU <mr.nicolas.grau@gmail.com>
2014-11-14 15:03:11 +0100

    Initial commit

--- app/src/main/java/com/nabillgram/app/MainActivity.java
+++ app/src/main/java/com/nabillgram/app/MainActivity.java
@@ -2,15 +2,10 @@ package com.nabillgram.app;
 
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 import android.widget.Button;
 
 public class MainActivity extends ActionBarActivity {
